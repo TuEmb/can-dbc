31dd43c6f0f3f4f87a8dfbf7321708d0371ac901
Tu Nguyen <tu.nguyen@ionmobility.com>
2024-06-20 16:33:10 +0700

    Remove condition checking length

--- src/lib.rs
+++ src/lib.rs
@@ -562,9 +562,6 @@ impl Message {
         offset: f32,
         msg: &[u8],
     ) -> Option<f32> {
-        if msg.len() < 8 {
-            return None;
-        }
         let msg64: u64 = if little_endian == ByteOrder::LittleEndian {
             <LittleEndian as byteorder::ByteOrder>::read_u64(msg)
         } else {
